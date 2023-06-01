8f6d005c3388aed74e4c35e20a427c9147b0258a
YazAtth <yasith.athan@gmail.com>
2023-06-01 09:51:54 +0100

    FEAT: Added class property to disable mongodb from returning object ids

--- MongoDbCollectionHandler.py
+++ MongoDbCollectionHandler.py
@@ -18,28 +18,42 @@ from pymongo import MongoClient
 
 
 class MongoDbCollectionHandler:
-    def __init__(self, uri, databaseName, collectionName):
+    def __init__(self, uri, databaseName, collectionName, getObjectIds=True):
         self.client = MongoClient(uri)
         self.db = self.client[databaseName]
         self.collection = self.db[collectionName]
+        self.getObjectIds = getObjectIds
 
 
     def getAllItems(self):
         json_objects = []
 
-        for item in self.collection.find({}):
-            json_objects.append(item)
+        if self.getObjectIds:
+            for item in self.collection.find({}):
+                json_objects.append(item)
+        else:
+            for item in self.collection.find({}, {"_id": 0}):
+                json_objects.append(item)
 
         return json_objects
 
     def findOne(self, query):
-        return self.collection.find_one(query)
+        if self.getObjectIds:
+            return self.collection.find_one(query)
+        else:
+            print("ran")
+            return self.collection.find_one(query, {"_id": 0})
+
 
     def findMany(self, query):
         json_objects = []
 
-        for item in self.collection.find(query):
-            json_objects.append(item)
+        if self.getObjectIds:
+            for item in self.collection.find(query):
+                json_objects.append(item)
+        else:
+            for item in self.collection.find(query, {"_id": 0}):
+                json_objects.append(item)
 
 
         return json_objects
--- playgrounds.py
+++ playgrounds.py
@@ -1,7 +1,7 @@
 from MongoDbCollectionHandler import MongoDbCollectionHandler
 
-mongodbCollectionHandler = MongoDbCollectionHandler("mongodb+srv://user:netninja@nodetutorial.d7env.mongodb.net/practicingDb?retryWrites=true&w=majority",
-                                "practicingDb", "dbpracticecollections2")
+mongodbCollectionHandler = MongoDbCollectionHandler(uri="mongodb+srv://user:netninja@nodetutorial.d7env.mongodb.net/practicingDb?retryWrites=true&w=majority",
+                                databaseName="practicingDb", collectionName="dbpracticecollections2", getObjectIds=False)
 
 
 # print(mongodbCollectionHandler.getAllItems())
@@ -15,7 +15,9 @@ mongodbCollectionHandler = MongoDbCollectionHandler("mongodb+srv://user:netninja
 # print(mongodbCollectionHandler.findOne({"title":"hello world"}))
 
 
-mongodbCollectionHandler.replaceAllItems([
-    {"title":"lmaoo"},
-    {"title":"ok lol"}
-])
+# mongodbCollectionHandler.replaceAllItems([
+#     {"title":"lmaoo"},
+#     {"title":"ok lol"}
+# ])
+
+print(mongodbCollectionHandler.findOne({"title": "lmaoo"}))
