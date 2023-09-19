b18985ac4737e5a23d44d559be689714733d6120
YazAtth <yasith.athan@gmail.com>
2023-09-19 20:30:46 +0100

    FEAT: Added list parsing to s3 file read

--- S3Client.py
+++ S3Client.py
@@ -1,16 +1,26 @@
 import boto3 as boto3
 
 
+def _csv_string_to_list(string):
+    return [url.strip('"') for url in string.split(',\n')]
+
+
 class S3Client:
 
     def __init__(self):
         self.s3_client = boto3.client('s3')
 
     # 'sample-cli-test-bucket'
-    def read_file_from_s3(self, bucket_name, file_name):
+    def read_file_from_s3(self, bucket_name, file_name, is_list=False):
         obj = self.s3_client.get_object(Bucket=bucket_name, Key=file_name)
         data = obj['Body'].read()
-        return str(data).split('\'')[1].replace("\\n", "\n")
+
+        data = str(data).split('\'')[1].replace("\\n", "\n")
+
+        if is_list:
+            return _csv_string_to_list(data)
+        else:
+            return data
 
     def write_to_s3_file(self, data_string, bucket_name, key_name):
         self.s3_client.put_object(
--- playgrounds.py
+++ playgrounds.py
@@ -1,7 +1,13 @@
 from S3Client import S3Client
+import csv
+from io import StringIO
+
 
 s3_client = S3Client()
 
-print(s3_client.read_file_from_s3(bucket_name="sample-cli-test-bucket", file_name="rss_feeds.txt"))
+input_string = s3_client.read_file_from_s3(bucket_name="sample-cli-test-bucket", file_name="rss_feeds.txt", is_list=True)
+
+print(input_string)
+
 
 
